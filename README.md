7c95bfd3a68e93527fa5561e39c7a7626090426f
Muhammad Talal Anwar <talalanwar@outlook.com>
2019-02-05 18:57:26 +0100

    allow sentry to get DSN from environment variable

--- README.md
+++ README.md
@@ -29,6 +29,6 @@ some service's config file, e.g. `proxy-server.conf`, `object-expirer.conf`,
 | Option | Required | Description |
 | --- | --- | --- |
 | `log_custom_handlers` | yes | Add `swift_sentry.sentry_logger` to this config option. Refer to Swift's [documentation](https://docs.openstack.org/swift/latest/admin_guide.html#custom-log-handlers) for more info regarding custom log handler hooks. |
-| `sentry_dsn` | yes | DSN (Data Source Name) for your Sentry Project. |
+| `sentry_dsn` | no | DSN (Data Source Name) for your Sentry Project. If `sentry_dsn` is not specified then the DSN value is read from the `SENTRY_DSN` environment variable. If both are configured then the environment variable has priority. |
 | `sentry_ignore_loggers` | no | A comma-separated list of loggers for Sentry to [ignore](https://docs.sentry.io/platforms/python/logging/#ignoring-a-logger). |
-| `sentry_log_level` | no | Report log records with a level higher than or equal to this value as events. Defaults to `ERROR`. You may want to set this value if you also want to send `INFO` or `DEBUG` messages. |
+| `sentry_log_level` | no | Log records with a level higher than or equal to this value are sent to Sentry. This value defaults to `ERROR`. You may want to set this value if you also want to send `INFO` or `DEBUG` messages. |
--- swift_sentry/__init__.py
+++ swift_sentry/__init__.py
@@ -1,19 +1,29 @@
+# -*- coding: utf-8 -*-
 import logging
+import os
 from sentry_sdk import init
 from sentry_sdk.integrations.logging import EventHandler, ignore_logger
 
 
 def sentry_logger(conf, name, log_to_console, log_route, fmt, logger, adapted_logger):
+    """
+    sentry_logger sets up a custom log handler hook for Swift.
+    Its parameters are the same as Swift’s get_logger function (as well as the
+    getLogger and LogAdapter object).
+    """
     sentry_dsn = conf.get("sentry_dsn", None)
     sentry_ignore_loggers = conf.get("sentry_ignore_loggers", None)
     sentry_log_level = getattr(
         logging, conf.get("sentry_log_level", "ERROR").upper(), logging.ERROR
     )
 
-    if sentry_dsn:
-        sentry_handler = EventHandler()
-        sentry_handler.setLevel(sentry_log_level)
-        logger.addHandler(sentry_handler)
+    if os.environ.get("SENTRY_DSN") or sentry_dsn:
+        # An exported environment variable 'SENTRY_DSN' has priority
+        # over the 'sentry_dsn' value defined in the config file
+        if os.environ.get("SENTRY_DSN"):
+            init()
+        else:
+            init(sentry_dsn)
 
         if sentry_ignore_loggers:
             ignore_loggers_list = [
@@ -22,4 +32,6 @@ def sentry_logger(conf, name, log_to_console, log_route, fmt, logger, adapted_lo
             for lgr in ignore_loggers_list:
                 ignore_logger(lgr)
 
-        init(sentry_dsn)
+        sentry_handler = EventHandler()
+        sentry_handler.setLevel(sentry_log_level)
+        logger.addHandler(sentry_handler)
